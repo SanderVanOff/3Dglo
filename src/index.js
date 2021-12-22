e5ad5dee26bd930c9cb301a961bed476e68a877a
SanderVanOff <7okay@mail.ru>
2021-12-23 00:10:22 +0500

    добавление нового модуля

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import timer from './modules/timer';
 import menu from './modules/menu';
 import modal from './modules/modal';
+import smoothScroll from './modules/smoothScroll';
 
 timer('22 december 2021');
 menu();
 modal();
+smoothScroll();
