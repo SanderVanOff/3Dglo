c8929114fd9a3e59d0291113c0fecb99021c92d4
SanderVanOff <7okay@mail.ru>
2021-12-26 20:28:51 +0500

    добавление домашнего дз

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import menu from './modules/menu';
 import modal from './modules/modal';
 import smoothScroll from './modules/smoothScroll';
 import calculator from './modules/calculator';
+import forms from './modules/forms';
 
 timer('22 december 2021');
 menu();
 modal();
 smoothScroll();
 calculator();
+forms();
